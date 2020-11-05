04a4cafbfa2a76f11e6c798f77b83bb7014e6cdc
koszuta <koszutan94@gmail.com>
2020-11-04 23:30:47 -0700

    2019 Day 7 part 2 complete

--- 2019/day7/main.go
+++ 2019/day7/main.go
@@ -1,184 +1,261 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-const minPhase1 = 0
-const maxPhase1 = 4
-const feedbackMode1 = false
+func intcodeCPU(instrs []int, in, out chan int, id string) {
+	relBase := 0
+	mem := make(map[uint64]int)
 
-const minPhase2 = 5
-const maxPhase2 = 9
-const feedbackMode2 = true
+	read := func(ptr, mode int) int {
+		var addr uint64
+		switch mode {
+		case 1:
+			return instrs[ptr]
+		case 0:
+			addr = uint64(instrs[ptr])
+		case 2:
+			addr = uint64(instrs[ptr] + relBase)
+		default:
+			log.Panicf("%s unknown read mode %d at %d\n", id, mode, ptr)
+		}
+		if addr < uint64(len(instrs)) {
+			return instrs[addr]
+		}
+		return mem[addr]
+	}
 
-// var program = []int{3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 42, 51, 60, 77, 94, 175, 256, 337, 418, 99999, 3, 9, 1001, 9, 4, 9, 102, 5, 9, 9, 1001, 9, 3, 9, 102, 5, 9, 9, 4, 9, 99, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 3, 9, 4, 9, 99, 3, 9, 101, 4, 9, 9, 1002, 9, 4, 9, 101, 5, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 5, 9, 101, 3, 9, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 99}
-var program = []int{3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0}
+	write := func(val, ptr, mode int) {
+		var addr uint64
+		switch mode {
+		case 0:
+			addr = uint64(instrs[ptr])
+		case 2:
+			addr = uint64(instrs[ptr] + relBase)
+		default:
+			log.Panicf("%s unknown write mode %d at %d\n", id, mode, ptr)
+		}
+		if addr < uint64(len(instrs)) {
+			instrs[addr] = val
+		} else {
+			mem[addr] = val
+		}
+	}
 
-func main() {
-	fmt.Printf("Go...\n")
+	exec3i := func(ptr, paramModes int, op func(int, int) int) int {
+		vals := [2]int{}
+		for i := 0; i < 2; i++ {
+			vals[i] = read(ptr+i, paramModes%10)
+			paramModes /= 10
+		}
+		write(op(vals[0], vals[1]), ptr+2, paramModes%10)
+		return ptr + 3
+	}
 
-	maxOutput := amplify(minPhase1, maxPhase1, feedbackMode1)
-	fmt.Printf("maxOutput=%d feedbackMode=%v\n", maxOutput, feedbackMode1)
+	addFunc := func(v1, v2 int) int {
+		return v1 + v2
+	}
+	mulFunc := func(v1, v2 int) int {
+		return v1 * v2
+	}
+	ltFunc := func(v1, v2 int) int {
+		if v1 < v2 {
+			return 1
+		}
+		return 0
+	}
+	eqFunc := func(v1, v2 int) int {
+		if v1 == v2 {
+			return 1
+		}
+		return 0
+	}
 
-	maxOutput = amplify(minPhase2, maxPhase2, feedbackMode2)
-	fmt.Printf("maxOutput=%d feedbackMode=%v\n", maxOutput, feedbackMode2)
+	ptr := 0
+	for {
+		paramModes := instrs[ptr] / 100
+		opCode := instrs[ptr] % 100
+		ptr++
+		switch opCode {
+
+		case 1: // add
+			ptr = exec3i(ptr, paramModes, addFunc)
+
+		case 2: // multiply
+			ptr = exec3i(ptr, paramModes, mulFunc)
+
+		case 3: // input
+			time.Sleep(time.Duration(1000000))
+			// log.Printf("%s getting input...", id)
+			v, ok := <-in
+			if !ok {
+				goto END
+			}
+			time.Sleep(time.Duration(1000000))
+			// log.Printf("%s ...input is %d", id, v)
+			write(v, ptr, paramModes%10)
+			ptr++
+
+		case 4: // output
+			time.Sleep(time.Duration(1000000))
+			v := read(ptr, paramModes%10)
+			// log.Printf("%s sending output...", id)
+			out <- v
+			time.Sleep(time.Duration(1000000))
+			// log.Printf("%s ...output is %d", id, v)
+			ptr++
+
+		case 5: // jump-if-true
+			mode := paramModes % 10
+			if read(ptr, mode) != 0 {
+				ptr = read(ptr+1, paramModes/10%10)
+			} else {
+				ptr += 2
+			}
+
+		case 6: // jump-if-false
+			mode := paramModes % 10
+			if read(ptr, mode) == 0 {
+				ptr = read(ptr+1, paramModes/10%10)
+			} else {
+				ptr += 2
+			}
+
+		case 7: // less than
+			ptr = exec3i(ptr, paramModes, ltFunc)
+
+		case 8: // equals
+			ptr = exec3i(ptr, paramModes, eqFunc)
+
+		case 9: // relative base offset
+			relBase += read(ptr, paramModes%10)
+			ptr++
+
+		case 99: // halt
+			goto END
+
+		default:
+			log.Println(instrs)
+			log.Panicf("%s encountered unknown intcode %d at index %d\n", id, instrs[ptr], ptr)
+		}
+	}
+END:
+	// log.Printf("stopping %s...\n", id)
+	close(out)
+	for range in {
+		// log.Printf("%s flushed input value %d...", id, input)
+	}
 }
 
-func amplify(minPhase int, maxPhase int, feedbackMode bool) int {
-	var ok bool
-	output := 0
-	maxOutput := 0
-	phases := make([]int, 0)
-	for i := minPhase; i <= maxPhase; i++ {
-		phases = append(phases, i)
-		for j := minPhase; j <= maxPhase; j++ {
-			if contains(j, phases) {
-				continue
+func startIntcodeCPU(instrs []int, in chan int, id string) chan int {
+	out := make(chan int)
+	go intcodeCPU(instrs, in, out, id)
+	return out
+}
+
+func parseProgram(fileName string) []int {
+	program := make([]int, 0, 0)
+	{
+		file, err := os.Open(fileName)
+		if err != nil {
+			log.Panicln(err)
+		}
+		programStr, err := bufio.NewReader(file).ReadString(0)
+		if err != nil && err != io.EOF {
+			log.Panicln(err)
+		}
+		for _, c := range strings.Split(programStr, ",") {
+			intcode, err := strconv.Atoi(c)
+			if err != nil {
+				log.Panicln(err)
 			}
-			phases = append(phases, j)
-			for k := minPhase; k <= maxPhase; k++ {
-				if contains(k, phases) {
-					continue
+			program = append(program, intcode)
+		}
+	}
+	return program
+}
+
+func amplify(phases, remaining, program []int, feedback bool, maxSignal int) int {
+	if len(remaining) == 0 {
+		inputs := make([]chan int, 0, len(phases))
+		outputs := make([]chan int, 0, len(phases))
+		for i := range phases {
+			in := make(chan int)
+			inputs = append(inputs, in)
+			out := startIntcodeCPU(program, in, fmt.Sprintf("cpu%d", i))
+			outputs = append(outputs, out)
+		}
+
+		// Seed each amplifier with its phase value
+		for i := 0; i < len(phases); i++ {
+			inputs[i] <- phases[i]
+		}
+
+		// Initial input for Amp A
+		inputs[0] <- 0
+
+		if feedback {
+			i := 1
+			for {
+				j := (len(phases) + i - 1) % len(phases)
+				output, ok := <-outputs[j]
+				if !ok {
+					break
 				}
-				phases = append(phases, k)
-				for l := minPhase; l <= maxPhase; l++ {
-					if contains(l, phases) {
-						continue
-					}
-					phases = append(phases, l)
-					for m := minPhase; m <= maxPhase; m++ {
-						if contains(m, phases) {
-							continue
-						}
-						phases = append(phases, m)
-
-						// for output != prev {
-						for q := 0; q < len(phases); q++ {
-							output, ok = intcode(phases[q], output)
-						}
-
-						if ok {
-							fmt.Printf("(%d, %d, %d, %d, %d)=%d\n", i, j, k, l, m, output)
-							if output > maxOutput {
-								maxOutput = output
-							}
-						}
-						// }
-
-						output = 0
-						phases = phases[:len(phases)-1]
-					}
-					phases = phases[:len(phases)-1]
-				}
-				phases = phases[:len(phases)-1]
+				maxSignal = output
+				inputs[i] <- output
+				i = (i + 1) % len(phases)
 			}
-			phases = phases[:len(phases)-1]
+		} else {
+			for i := 1; i < len(phases); i++ {
+				inputs[i] <- <-outputs[i-1]
+			}
+			maxSignal = <-outputs[4]
+		}
+		for _, in := range inputs {
+			close(in)
+		}
+		log.Printf("%v -> %d\n", phases, maxSignal)
+	}
+	for i, p := range remaining {
+		phases = append(phases, p)
+		newRemaining := make([]int, 0, 0)
+		newRemaining = append(newRemaining, remaining[:i]...)
+		newRemaining = append(newRemaining, remaining[i+1:]...)
+		max := amplify(phases, newRemaining, program, feedback, maxSignal)
+		if max > maxSignal {
+			maxSignal = max
 		}
 		phases = phases[:len(phases)-1]
 	}
-
-	return maxOutput
+	return maxSignal
 }
 
-func intcode(args ...int) (int, bool) {
-	prog := make([]int, len(program))
-	copy(prog, program)
+func main() {
+	fileName := flag.String("file", "input.txt", "input file")
+	flag.Parse()
+	program := parseProgram(*fileName)
 
-	var output int
-	hasOutput := false
-
-	opSize := 0
-	argsIndex := 0
-	for i := 0; prog[i] != 99; i += opSize {
-		// fmt.Printf("%d ", prog[i])
-
-		opCode := prog[i] % 100
-		modes := prog[i] / 100
-
-		var numOpParams int
-		allowImmediateMode := true
-		switch opCode {
-		case 1, 2, 7, 8:
-			opSize = 4
-			numOpParams = 2
-		case 5, 6:
-			opSize = 3
-			numOpParams = 2
-		case 3:
-			allowImmediateMode = false
-			fallthrough
-		default:
-			opSize = 2
-			numOpParams = 1
-		}
-
-		opParams := make([]int, numOpParams)
-		for j := 0; j < numOpParams; j, modes = j+1, modes/10 {
-			if allowImmediateMode && modes%10 == 0 {
-				opParams[j] = prog[prog[i+j+1]]
-				// fmt.Printf("[%d]=%d ", prog[i+j+1], opParams[j])
-			} else {
-				opParams[j] = prog[i+j+1]
-				// fmt.Printf("%d ", opParams[j])
-			}
-		}
-
-		switch opCode {
-		case 1:
-			prog[prog[i+3]] = opParams[0] + opParams[1]
-			// fmt.Printf("\n[%d]<-%d\n", prog[i+3], prog[prog[i+3]])
-		case 2:
-			prog[prog[i+3]] = opParams[0] * opParams[1]
-			// fmt.Printf("\n[%d]<-%d\n", prog[i+3], prog[prog[i+3]])
-		case 3:
-			for argsIndex >= len(args) {}
-			prog[opParams[0]] = args[argsIndex]
-			argsIndex++
-			// fmt.Printf("\n[%d]<-%d\n", opParams[0], input)
-		case 4:
-			output = opParams[0]
-			hasOutput = true
-			// fmt.Printf("\n>>> %d\n", output)
-		case 5:
-			// fmt.Printf("\n")
-			if opParams[0] != 0 {
-				i = opParams[1]
-				opSize = 0
-				// fmt.Printf("Go to %d\n", i)
-			}
-		case 6:
-			// fmt.Printf("\n")
-			if opParams[0] == 0 {
-				i = opParams[1]
-				opSize = 0
-				// fmt.Printf("Go to %d\n", i)
-			}
-		case 7:
-			if opParams[0] < opParams[1] {
-				prog[prog[i+3]] = 1
-			} else {
-				prog[prog[i+3]] = 0
-			}
-			// fmt.Printf("\n[%d]<-%d\n", prog[i+3], prog[prog[i+3]])
-		case 8:
-			if opParams[0] == opParams[1] {
-				prog[prog[i+3]] = 1
-			} else {
-				prog[prog[i+3]] = 0
-			}
-			// fmt.Printf("\n[%d]<-%d\n", prog[i+3], prog[prog[i+3]])
-		}
+	phases := make([]int, 0, 0)
+	remaining := make([]int, 0, 0)
+	for i := 0; i < 5; i++ {
+		remaining = append(remaining, i)
 	}
+	log.Printf("max signal without feedback is %d\n\n", amplify(phases, remaining, program, false, 0))
 
-	return output, hasOutput
-}
-
-func contains(target int, arr []int) bool {
-	for _, i := range arr {
-		if i == target {
-			return true
-		}
+	phases = make([]int, 0, 0)
+	remaining = make([]int, 0, 0)
+	for i := 5; i < 10; i++ {
+		remaining = append(remaining, i)
 	}
-	return false
+	log.Printf("max signal with feedback is %d\n", amplify(phases, remaining, program, true, 0))
 }
