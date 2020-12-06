a3068236fdb678e03e0e3454a6305e5d161da480
koszuta <koszutan94@gmail.com>
2020-12-05 22:55:34 -0700

    2020 Day 6 complete

--- 2020/day06/part1/main.go
+++ 2020/day06/part1/main.go
@@ -1,5 +1,41 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
+/*
+ *   --- Day 6: Custom Customs ---
+ *         --- Part Two ---
+ *
+ *   https://adventofcode.com/2020/day/6
+ */
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	commonChars := make(map[rune]struct{})
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Groups are separated by an empty line
+		if line == "" {
+			// Add the common char count to the sum
+			sum += len(commonChars)
+			commonChars = make(map[rune]struct{})
+
+		} else {
+			// Add this line's chars to the set of common chars
+			for _, c := range line {
+				commonChars[c] = struct{}{}
+			}
+		}
+	}
+
+	log.Printf("the sum of the counts is %d\n", sum)
 }
--- 2020/day06/part2/main.go
+++ 2020/day06/part2/main.go
@@ -1,5 +1,53 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
 
+/*
+ *   --- Day 6: Custom Customs ---
+ *         --- Part Two ---
+ *
+ *   https://adventofcode.com/2020/day/6#part2
+ */
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	lines := make([]string, 0, 0)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Groups are separated by an empty line
+		if line == "" {
+			count := len(lines[0])
+
+			// Iterate the chars in the first line
+			for _, c := range lines[0] {
+				// Check that each other line contains the char
+				for i := 1; i < len(lines); i++ {
+					if !strings.Contains(lines[i], string(c)) {
+						// Reduce the common char count if the char isn't present in a line
+						count--
+						break
+					}
+				}
+			}
+
+			// Add the common char count to the sum
+			sum += count
+			lines = make([]string, 0, 0)
+
+		} else {
+			lines = append(lines, line)
+		}
+	}
+
+	log.Printf("the sum of the counts is %d\n", sum)
 }
