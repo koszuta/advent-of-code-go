c98b70a70327654488c1e023039f33f238ea07e2
koszuta <koszutan94@gmail.com>
2020-12-09 00:02:07 -0700

    2020 Day 9 complete

--- 2020/day09/part1/main.go
+++ 2020/day09/part1/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
 
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	// Parse the input as a list of integers
+	nums := make([]int, 0, 0)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		nums = append(nums, num)
+	}
+
+OUTER:
+	for i := 25; i < len(nums); i++ {
+		num := nums[i]
+		for j := i - 25; j < i; j++ {
+			for k := j + 1; k < i; k++ {
+				if num == nums[j]+nums[k] {
+					continue OUTER
+				}
+			}
+		}
+		log.Printf("the first invalid number is %d\n", num)
+		break
+	}
 }
--- 2020/day09/part2/main.go
+++ 2020/day09/part2/main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
 
+func partOne(nums []int) int {
+OUTER:
+	for i := 25; i < len(nums); i++ {
+		num := nums[i]
+		for j := i - 25; j < i; j++ {
+			for k := j + 1; k < i; k++ {
+				if num == nums[j]+nums[k] {
+					continue OUTER
+				}
+			}
+		}
+		return num
+	}
+	return 0
+}
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	// Parse the input as a list of integers
+	nums := make([]int, 0, 0)
+	for scanner.Scan() {
+		num, _ := strconv.Atoi(scanner.Text())
+		nums = append(nums, num)
+	}
+
+	// Get the answer from Part One
+	answer := partOne(nums)
+
+	// Find a contiguous set of numbers which sum to the answer from Part One
+	sum := 0
+	set := make([]int, 0, 0)
+OUTER:
+	for i := 0; i < len(nums); i++ {
+		for j := i; j < len(nums); j++ {
+			num := nums[j]
+			if sum+num > answer {
+				// If adding this number would go past the answer,
+				// reset everything, and check from the next starting number
+				sum = 0
+				set = make([]int, 0, 0)
+				continue OUTER
+			} else {
+				// Otherwise, add this number to the set
+				sum += num
+				set = append(set, num)
+
+				// If we hit the answer exactly, stop checking
+				if sum == answer {
+					break OUTER
+				}
+			}
+		}
+	}
+
+	// The "encryption weakness" is min+max from the set
+	min, max := set[0], set[0]
+	for _, num := range set {
+		if num < min {
+			min = num
+		}
+		if num > max {
+			max = num
+		}
+	}
+
+	log.Printf("the encryption weakness is %d\n", min+max)
 }
