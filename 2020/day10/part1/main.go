82ab3b918b58753d9ac82acce1aa4b6c07387c45
koszuta <koszutan94@gmail.com>
2020-12-10 01:12:13 -0700

    2020 Day 10 complete

--- 2020/day10/part1/main.go
+++ 2020/day10/part1/main.go
@@ -1,5 +1,45 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	// Build a list of joltage adapters
+	jolts := make([]int, 0, 0)
+	for scanner.Scan() {
+		jolt, _ := strconv.Atoi(scanner.Text())
+		jolts = append(jolts, jolt)
+	}
+
+	// Sort the jolts
+	sort.Ints(jolts)
+
+	// Add the built-in joltage adapter
+	// which is 3 jolts higher than the highest-rated adapter
+	maxJolts := jolts[len(jolts)-1]
+	jolts = append(jolts, maxJolts+3)
+
+	// Count the number of 1 and 3 jolt differences between consecutive adapters
+	prev, oneDiff, threeDiff := 0, 0, 0
+	for _, jolt := range jolts {
+		switch jolt - prev {
+		case 1:
+			oneDiff++
+		case 3:
+			threeDiff++
+		}
+		prev = jolt
+	}
+
+	log.Printf("there are %d differences of 1 jolt and %d differences of 3 jolts\n", oneDiff, threeDiff)
+	log.Printf("answer: %d\n", oneDiff*threeDiff)
 }
--- 2020/day10/part2/main.go
+++ 2020/day10/part2/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	// Build a list of joltage adapters
+	jolts := make([]int, 0, 0)
+	for scanner.Scan() {
+		jolt, _ := strconv.Atoi(scanner.Text())
+		jolts = append(jolts, jolt)
+	}
+
+	// Add the outlet
+	jolts = append(jolts, 0)
+
+	// Sort the jolts
+	sort.Ints(jolts)
+
+	// Add the built-in joltage adapter
+	// which is 3 jolts higher than the highest-rated adapter
+	builtInAdapter := jolts[len(jolts)-1] + 3
+	jolts = append(jolts, builtInAdapter)
+
+	// Find the number of unique paths from the outlet to the built-in adapter
+	pathCounts := make(map[int]int)
+	pathCounts[0] = 1
+	for i := 1; i < len(jolts); i++ {
+		sum := 0
+		for j := i - 1; j >= 0 && jolts[j] >= jolts[i]-3; j-- {
+			sum += pathCounts[jolts[j]]
+		}
+		pathCounts[jolts[i]] = sum
+	}
+
+	paths := pathCounts[builtInAdapter]
+	log.Printf("the total number of distinct ways you can arrange the adapters is %d\n", paths)
 }
