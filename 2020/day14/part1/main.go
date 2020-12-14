43e1104c76258318a55cc56f8a368f81154a1807
koszuta <koszutan94@gmail.com>
2020-12-14 01:08:10 -0700

    2020 Day 14 complete

--- 2020/day14/part1/main.go
+++ 2020/day14/part1/main.go
@@ -1,5 +1,63 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+/*
+ *   --- Day 14: Docking Data ---
+ *         --- Part One ---
+ *
+ *   https://adventofcode.com/2020/day/14
+ */
+
+func applyMask(val int, mask string) int {
+	newVal := 0
+	for i, maskBit := range mask {
+		newVal <<= 1
+		switch maskBit {
+		case '0': // push a 0
+		case '1': // push a 1
+			newVal++
+		case 'X': // leave the bit unchanged
+			bit := (val >> (35 - i)) & 1
+			newVal += bit
+		}
+	}
+	return newVal
+}
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	var mask string
+	mem := make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "mask = ") {
+			mask = line[7:]
+
+		} else {
+			parts := strings.Split(line, " = ")
+			a := parts[0]
+			address, _ := strconv.Atoi(a[4 : len(a)-1])
+			value, _ := strconv.Atoi(parts[1])
+
+			// Apply the mask to the value and store it in memory at the address
+			mem[address] = applyMask(value, mask)
+		}
+	}
+
+	sum := 0
+	for _, v := range mem {
+		sum += v
+	}
+	log.Printf("the sum of all values in memory is %d\n", sum)
 }
--- 2020/day14/part2/main.go
+++ 2020/day14/part2/main.go
@@ -1,5 +1,77 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
 
+/*
+ *   --- Day 14: Docking Data ---
+ *         --- Part Two ---
+ *
+ *   https://adventofcode.com/2020/day/14#part2
+ */
+
+var mem map[int]int
+
+func applyMask(val int, mask string) []rune {
+	newVal := []rune(mask)
+	for i, c := range mask {
+		if c == '0' {
+			bit := (val >> (35 - i)) & 1
+			newVal[i] = '0' + rune(bit)
+		}
+	}
+	return newVal
+}
+
+func write(address []rune, val, depth int) {
+	if depth == 36 {
+		a, _ := strconv.ParseUint(string(address), 2, 36)
+		mem[int(a)] = val
+
+	} else if address[depth] == 'X' {
+		address[depth] = '0'
+		write(address, val, depth+1)
+		address[depth] = '1'
+		write(address, val, depth+1)
+		address[depth] = 'X'
+
+	} else {
+		write(address, val, depth+1)
+	}
+}
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	var mask string
+	mem = make(map[int]int)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if strings.HasPrefix(line, "mask = ") {
+			mask = line[7:]
+
+		} else {
+			parts := strings.Split(line, " = ")
+
+			address, _ := strconv.Atoi(parts[0][4 : len(parts[0])-1])
+			value, _ := strconv.Atoi(parts[1])
+
+			maskedAddr := applyMask(address, mask)
+			write(maskedAddr, value, 0)
+		}
+	}
+
+	sum := 0
+	for _, v := range mem {
+		sum += v
+	}
+	log.Printf("the sum of all values in memory is %d\n", sum)
 }
