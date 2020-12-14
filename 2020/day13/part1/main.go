a7ee1faa09fdfaeb0e9af38a7522841081dbd880
koszuta <koszutan94@gmail.com>
2020-12-14 01:00:28 -0700

    2020 Day 13 complete

--- 2020/day13/part1/main.go
+++ 2020/day13/part1/main.go
@@ -2,32 +2,54 @@ package main
 
 import (
 	"bufio"
+	"log"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type instruction struct {
-	op  string
-	arg int
-}
+/*
+ *   --- Day 13: Shuttle Search ---
+ *          --- Part One ---
+ *
+ *   https://adventofcode.com/2020/day/13
+ */
 
 func main() {
 	// Puzzle input
 	file, _ := os.Open("../input.txt")
 	scanner := bufio.NewScanner(file)
 
-	// Parse the instructions
-	instructions := make([]instruction, 0, 0)
-	for scanner.Scan() {
-		// Instructions are composed of an operation and an argument
-		// Operations: acc, jmp, nop
-		// Arguments: signed integers
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
-		op := parts[0]
-		arg, _ := strconv.Atoi(parts[1])
-		instructions = append(instructions, instruction{op, arg})
+	// The first line of the input is the earliest time at which you could depart
+	var time int
+	{
+		scanner.Scan()
+		time, _ = strconv.Atoi(scanner.Text())
 	}
 
+	// The second line is a list of bus IDs
+	// Entries that show x are not used
+	buses := make([]int, 0, 0)
+	{
+		scanner.Scan()
+		for _, id := range strings.Split(scanner.Text(), ",") {
+			if id != "x" {
+				busID, _ := strconv.Atoi(id)
+				buses = append(buses, busID)
+			}
+		}
+	}
+
+	// Buses depart at multiples of their ID
+	// Find the bus which leaves the soonest after the given time
+	minWait, minBusID := buses[0], buses[0]
+	for _, busID := range buses {
+		wait := busID - time%busID
+		if wait < minWait {
+			minWait = wait
+			minBusID = busID
+		}
+	}
+
+	log.Printf("the ID of the earliest bus multiplied by the time needed to wait for that bus is %d\n", minWait*minBusID)
 }
--- 2020/day13/part2/main.go
+++ 2020/day13/part2/main.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
 
+/*
+ *   --- Day 13: Shuttle Search ---
+ *          --- Part Two ---
+ *
+ *   https://adventofcode.com/2020/day/13#part2
+ */
+
+type bus struct {
+	id, offset int
+}
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	scanner := bufio.NewScanner(file)
+
+	// The first line of the input isn't used in Part Two
+	scanner.Scan()
+
+	// The second line is a list of bus IDs
+	// Store both their ID and index in the list
+	buses := make([]bus, 0, 0)
+	{
+		scanner.Scan()
+		for i, id := range strings.Split(scanner.Text(), ",") {
+			if id != "x" {
+				busID, _ := strconv.Atoi(id)
+				buses = append(buses, bus{busID, i})
+			}
+		}
+	}
+
+	// Sort the buses by ID in descending order
+	sort.Slice(buses, func(i, j int) bool {
+		return buses[i].id > buses[j].id
+	})
+
+	// Use the Chinese Remainder Theorem
+	time, jump := 0, 1
+	for i := 0; i < len(buses); i++ {
+		for (time+buses[i].offset)%buses[i].id != 0 {
+			time += jump
+		}
+		jump *= buses[i].id
+	}
+
+	log.Printf("the earliest time where all of the listed bus IDs depart at offsets matching their positions in the list is %d\n", time)
 }
