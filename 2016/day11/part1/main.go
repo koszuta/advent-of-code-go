1dfa3e48a76a9a6413deedd457a8710db1457b7e
koszuta <koszutan94@gmail.com>
2021-08-07 14:57:58 -0600

    2016 day 11

---
+++ 2016/day11/part1/main.go
@@ -0,0 +1,186 @@
+package main
+
+import (
+	"advent-of-code-go/2016/day11/part1/rtg"
+	"fmt"
+	"time"
+)
+
+/*
+ *   --- Day 11: Radioisotope Thermoelectric Generators ---
+ *                      --- Part One ---
+ *
+ *   https://adventofcode.com/2016/day/11
+ */
+
+var (
+	hashGraph     map[rtg.Hash]map[rtg.Hash]struct{}
+	startingState rtg.State
+)
+
+func init() {
+	hashGraph = make(map[rtg.Hash]map[rtg.Hash]struct{})
+
+	// startingState = rtg.NewState(0, [rtg.NElements]int{1, 2}, [rtg.NElements]int{0, 0})
+	// example(startingState)
+	startingState = rtg.NewState(0, [rtg.NElements]int{0, 0, 0, 0, 0}, [rtg.NElements]int{1, 1, 0, 0, 0})
+
+	fmt.Println("starting state:")
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+}
+
+func main() {
+	defer (func(start time.Time) {
+		fmt.Printf("... took %v\n", time.Since(start))
+	})(time.Now())
+
+	doMove(0, &startingState)
+
+	dist := bfs(startingState.Hash())
+	fmt.Println("depth:", dist)
+}
+
+type hashNode struct {
+	hash rtg.Hash
+	dist int
+}
+
+func bfs(root rtg.Hash) int {
+	visited := make(map[rtg.Hash]struct{})
+	queue := make([]hashNode, 0)
+
+	visited[root] = struct{}{}
+	queue = append(queue, hashNode{root, 0})
+
+	for len(queue) > 0 {
+		v := queue[0]
+		queue = queue[1:]
+		if v.hash == rtg.FinalStateHash {
+			return v.dist
+		}
+		for w := range hashGraph[v.hash] {
+			_, seen := visited[w]
+			if !seen {
+				queue = append(queue, hashNode{w, v.dist + 1})
+				visited[w] = struct{}{}
+			}
+		}
+	}
+	return -1
+}
+
+func doMove(depth int, currentState *rtg.State) {
+	if currentState.Hash() == rtg.FinalStateHash {
+		return
+	}
+
+	var nextState *rtg.State
+	for i := 0; i < rtg.NElements; i++ {
+		nextState = currentState.MoveChipUp(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move microchip %d up:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		nextState = currentState.MoveChipDown(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move microchip %d down:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		nextState = currentState.MoveGenUp(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move generator %d up:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		nextState = currentState.MoveGenDown(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move generator %d down:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		nextState = currentState.MovePairUp(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move gen and chip %d up:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		nextState = currentState.MovePairDown(i)
+		if storeMove(currentState, nextState, fmt.Sprintf("move gen and chip %d down:", i)) {
+			doMove(depth+1, nextState)
+		}
+
+		for j := i + 1; j < rtg.NElements; j++ {
+			nextState = currentState.MoveChipsUp(i, j)
+			if storeMove(currentState, nextState, fmt.Sprintf("move microchips %d and %d up:", i, j)) {
+				doMove(depth+1, nextState)
+			}
+
+			nextState = currentState.MoveChipsDown(i, j)
+			if storeMove(currentState, nextState, fmt.Sprintf("move microchips %d and %d down:", i, j)) {
+				doMove(depth+1, nextState)
+			}
+
+			nextState = currentState.MoveGensUp(i, j)
+			if storeMove(currentState, nextState, fmt.Sprintf("move generators %d and %d up:", i, j)) {
+				doMove(depth+1, nextState)
+			}
+
+			nextState = currentState.MoveGensDown(i, j)
+			if storeMove(currentState, nextState, fmt.Sprintf("move generators %d and %d down:", i, j)) {
+				doMove(depth+1, nextState)
+			}
+		}
+	}
+}
+
+func storeMove(currentState, nextState *rtg.State, message string) bool {
+	if nextState != nil {
+		currentHash := currentState.Hash()
+		_, found := hashGraph[currentHash]
+		if !found {
+			hashGraph[currentHash] = make(map[rtg.Hash]struct{})
+		}
+		nextHash := nextState.Hash()
+		hashGraph[currentHash][nextHash] = struct{}{}
+		_, found = hashGraph[nextHash]
+		return !found
+	}
+	return false
+}
+
+func example(startingState rtg.State) {
+	fmt.Println("example solution:")
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipUp(0)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MovePairUp(0)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipDown(0)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipDown(0)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipsUp(0, 1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipsUp(0, 1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipsUp(0, 1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipDown(0)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveGensUp(0, 1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipDown(1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+	startingState = *startingState.MoveChipsUp(0, 1)
+	fmt.Println(startingState.ToString())
+	fmt.Println()
+}
---
+++ 2016/day11/part1/rtg/State.go
@@ -0,0 +1,316 @@
+package rtg
+
+import "fmt"
+
+const (
+	// NElements = 2
+	NElements = 5
+	NFloors   = 4
+)
+
+type State struct {
+	Elevator   int
+	Generators [NElements]int
+	Microchips [NElements]int
+}
+
+type Hash struct {
+	Elevator                      int
+	NPairs, NLoneGens, NLoneChips [NFloors]int
+}
+
+var (
+	FinalState     State
+	FinalStateHash Hash
+)
+
+func init() {
+	var allTopFloor [NElements]int
+	for i := 0; i < NElements; i++ {
+		allTopFloor[i] = NFloors - 1
+	}
+	FinalState = NewState(NFloors-1, allTopFloor, allTopFloor)
+	FinalStateHash = FinalState.Hash()
+}
+
+func NewState(elevator int, generators, microchips [NElements]int) State {
+	return State{Elevator: elevator, Generators: generators, Microchips: microchips}
+}
+
+func (state *State) Copy() State {
+	return NewState(state.Elevator, state.Generators, state.Microchips)
+}
+
+func (state *State) Hash() (hash Hash) {
+	hash.Elevator = state.Elevator
+	for i := 0; i < NElements; i++ {
+		genFloor, chipFloor := state.Generators[i], state.Microchips[i]
+		if genFloor == chipFloor {
+			hash.NPairs[genFloor]++
+		} else {
+			hash.NLoneGens[genFloor]++
+			hash.NLoneChips[chipFloor]++
+		}
+	}
+	return hash
+}
+
+func (state *State) ToString() string {
+	s := ""
+	for floor := NFloors - 1; floor >= 0; floor-- {
+		row := fmt.Sprintf("F%d", floor+1)
+		if state.Elevator == floor {
+			row += " E "
+		} else {
+			row += " . "
+		}
+		for element := 0; element < NElements; element++ {
+			if state.Generators[element] == floor {
+				row += fmt.Sprintf(" G%d", element)
+			} else {
+				row += " . "
+			}
+			if state.Microchips[element] == floor {
+				row += fmt.Sprintf(" M%d", element)
+			} else {
+				row += " . "
+			}
+		}
+		s += row
+		if floor > 0 {
+			s += "\n"
+		}
+	}
+	return s
+}
+
+func (state *State) MoveGenUp(element int) *State {
+	genFloor := state.Generators[element]
+	if state.Elevator != genFloor || state.Elevator == NFloors-1 {
+		return nil
+	}
+	aboveIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] > state.Elevator || state.Microchips[e] > state.Elevator {
+			aboveIsEmpty = false
+			break
+		}
+	}
+	if aboveIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator++
+	newState.Generators[element] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveGenDown(element int) *State {
+	genFloor := state.Generators[element]
+	if state.Elevator != genFloor || state.Elevator == 0 {
+		return nil
+	}
+	belowIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] < state.Elevator || state.Microchips[e] < state.Elevator {
+			belowIsEmpty = false
+			break
+		}
+	}
+	if belowIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator--
+	newState.Generators[element] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveChipUp(element int) *State {
+	chipFloor := state.Microchips[element]
+	if state.Elevator != chipFloor || state.Elevator == NFloors-1 {
+		return nil
+	}
+	aboveIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] > state.Elevator || state.Microchips[e] > state.Elevator {
+			aboveIsEmpty = false
+			break
+		}
+	}
+	if aboveIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator++
+	newState.Microchips[element] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveChipDown(element int) *State {
+	chipFloor := state.Microchips[element]
+	if state.Elevator != chipFloor || state.Elevator == 0 {
+		return nil
+	}
+	belowIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] < state.Elevator || state.Microchips[e] < state.Elevator {
+			belowIsEmpty = false
+			break
+		}
+	}
+	if belowIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator--
+	newState.Microchips[element] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MovePairUp(element int) *State {
+	genFloor, chipFloor := state.Generators[element], state.Microchips[element]
+	if state.Elevator != genFloor || state.Elevator != chipFloor || state.Elevator == NFloors-1 {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator++
+	newState.Generators[element] = newState.Elevator
+	newState.Microchips[element] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MovePairDown(element int) *State {
+	genFloor, chipFloor := state.Generators[element], state.Microchips[element]
+	if state.Elevator != genFloor || state.Elevator != chipFloor || state.Elevator == 0 {
+		return nil
+	}
+	belowIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] < state.Elevator || state.Microchips[e] < state.Elevator {
+			belowIsEmpty = false
+			break
+		}
+	}
+	if belowIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator--
+	newState.Generators[element], newState.Microchips[element] = newState.Elevator, newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveGensUp(element1, element2 int) *State {
+	genFloor1, genFloor2 := state.Generators[element1], state.Generators[element2]
+	if state.Elevator != genFloor1 || state.Elevator != genFloor2 || state.Elevator == NFloors-1 {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator++
+	newState.Generators[element1] = newState.Elevator
+	newState.Generators[element2] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveGensDown(element1, element2 int) *State {
+	genFloor1, genFloor2 := state.Generators[element1], state.Generators[element2]
+	if state.Elevator != genFloor1 || state.Elevator != genFloor2 || state.Elevator == 0 {
+		return nil
+	}
+	belowIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] < state.Elevator || state.Microchips[e] < state.Elevator {
+			belowIsEmpty = false
+			break
+		}
+	}
+	if belowIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator--
+	newState.Generators[element1] = newState.Elevator
+	newState.Generators[element2] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveChipsUp(element1, element2 int) *State {
+	chipFloor1, chipFloor2 := state.Microchips[element1], state.Microchips[element2]
+	if state.Elevator != chipFloor1 || state.Elevator != chipFloor2 || state.Elevator == NFloors-1 {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator++
+	newState.Microchips[element1] = newState.Elevator
+	newState.Microchips[element2] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) MoveChipsDown(element1, element2 int) *State {
+	chipFloor1, chipFloor2 := state.Microchips[element1], state.Microchips[element2]
+	if state.Elevator != chipFloor1 || state.Elevator != chipFloor2 || state.Elevator == 0 {
+		return nil
+	}
+	belowIsEmpty := true
+	for e := 0; e < NElements; e++ {
+		if state.Generators[e] < state.Elevator || state.Microchips[e] < state.Elevator {
+			belowIsEmpty = false
+			break
+		}
+	}
+	if belowIsEmpty {
+		return nil
+	}
+	newState := state.Copy()
+	newState.Elevator--
+	newState.Microchips[element1] = newState.Elevator
+	newState.Microchips[element2] = newState.Elevator
+	if !newState.Valid() {
+		return nil
+	}
+	return &newState
+}
+
+func (state *State) Valid() bool {
+	for element := 0; element < NElements; element++ {
+		genFloor, chipFloor := state.Generators[element], state.Microchips[element]
+		if genFloor < 0 || chipFloor < 0 || genFloor >= NFloors || chipFloor >= NFloors {
+			return false
+		}
+		if genFloor != chipFloor {
+			hash := state.Hash()
+			if hash.NPairs[chipFloor] != 0 || hash.NLoneGens[chipFloor] != 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
