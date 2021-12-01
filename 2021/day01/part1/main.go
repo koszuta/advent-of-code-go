5d0f758651974a53c97541be56956dc7ece1966c
koszuta <koszutan94@gmail.com>
2021-11-30 23:07:10 -0700

    2021 day 1

---
+++ 2021/day01/part1/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+/*
+ *   --- Day 1: Sonar Sweep ---
+ *        --- Part One ---
+ *
+ *   https://adventofcode.com/2021/day/1
+ */
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	defer file.Close()
+
+	depths := make([]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		depth, err := strconv.Atoi(line)
+		if err != nil {
+			log.Panicln(err)
+		}
+		depths = append(depths, depth)
+	}
+
+	nIncreases := 0
+	for i := 1; i < len(depths); i++ {
+		if depths[i] > depths[i-1] {
+			nIncreases++
+		}
+	}
+	log.Println("the number of measurements larger than the previous measurement is", nIncreases)
+}
---
+++ 2021/day01/part2/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+/*
+ *   --- Day 1: Sonar Sweep ---
+ *        --- Part Two ---
+ *
+ *   https://adventofcode.com/2021/day/1#part2
+ */
+
+func main() {
+	// Puzzle input
+	file, _ := os.Open("../input.txt")
+	defer file.Close()
+
+	depths := make([]int, 0)
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		depth, err := strconv.Atoi(line)
+		if err != nil {
+			log.Panicln(err)
+		}
+		depths = append(depths, depth)
+	}
+
+	nIncreases := 0
+	prevSum := depths[0] + depths[1] + depths[2]
+	for i := 3; i < len(depths); i++ {
+		newSum := prevSum - depths[i-3] + depths[i]
+		if newSum > prevSum {
+			nIncreases++
+		}
+		prevSum = newSum
+	}
+	log.Println("the number of sums larger than the previous sum is", nIncreases)
+}
