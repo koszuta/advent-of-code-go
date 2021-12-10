cbfb23289228f1bf46d2dc8d9fa44d5d71c97a11
koszuta <koszutan94@gmail.com>
2021-12-09 23:14:37 -0700

    2021 day 10

---
+++ 2021/day10/part1/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+/*
+ *   --- Day 10: Syntax Scoring ---
+ *        --- Part One ---
+ *
+ *   https://adventofcode.com/2021/day/10
+ */
+
+var (
+	illegalCharScores map[rune]int
+
+	openingChars = "([{<"
+	closingChars = ")]}>"
+)
+
+func init() {
+	illegalCharScores = make(map[rune]int)
+	illegalCharScores[')'] = 3
+	illegalCharScores[']'] = 57
+	illegalCharScores['}'] = 1197
+	illegalCharScores['>'] = 25137
+}
+
+func main() {
+	defer func(t time.Time) {
+		log.Println("took", time.Since(t))
+	}(time.Now())
+
+	score := doPart1()
+	log.Println("the total syntax error score is", score)
+}
+
+func doPart1() int {
+	file, _ := os.Open("../input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	sum := 0
+	for scanner.Scan() {
+		char := checkChunks(scanner.Text())
+		if char != 0 {
+			sum += illegalCharScores[char]
+		}
+	}
+	return sum
+}
+
+func checkChunks(line string) rune {
+	var charToMatch rune
+	chars := make([]rune, 0)
+	for _, r := range line {
+		if strings.ContainsRune(openingChars, r) {
+			chars = append(chars, r)
+
+		} else if strings.ContainsRune(closingChars, r) {
+			charToMatch, chars = chars[len(chars)-1], chars[:len(chars)-1]
+			expectedOpening := getOpeningChar(r)
+			if charToMatch != expectedOpening {
+				return r
+			}
+		} else {
+			log.Panicln("unhandled char", string(r))
+		}
+	}
+	return 0
+}
+
+func getOpeningChar(r rune) rune {
+	return rune(openingChars[strings.IndexRune(closingChars, r)])
+}
---
+++ 2021/day10/part2/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"sort"
+	"strings"
+	"time"
+)
+
+/*
+ *   --- Day 10: Syntax Scoring ---
+ *        --- Part Two ---
+ *
+ *   https://adventofcode.com/2021/day/10#part2
+ */
+
+var (
+	unmatchedCharScores map[rune]int
+
+	openingChars = "([{<"
+	closingChars = ")]}>"
+)
+
+func init() {
+	unmatchedCharScores = make(map[rune]int)
+	unmatchedCharScores[')'] = 1
+	unmatchedCharScores[']'] = 2
+	unmatchedCharScores['}'] = 3
+	unmatchedCharScores['>'] = 4
+}
+
+func main() {
+	defer func(t time.Time) {
+		log.Println("took", time.Since(t))
+	}(time.Now())
+
+	score := doPart2()
+	log.Println("the middle score is", score)
+}
+
+func doPart2() int {
+	file, _ := os.Open("../input.txt")
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	scores := make([]int, 0)
+	for scanner.Scan() {
+		char, unmatchedChars := checkChunks(scanner.Text())
+		if char == 0 {
+			score := 0
+			for i := len(unmatchedChars) - 1; i >= 0; i-- {
+				score *= 5
+				openingChar := unmatchedChars[i]
+				score += unmatchedCharScores[getClosingChar(openingChar)]
+			}
+			scores = append(scores, score)
+		}
+	}
+	sort.Slice(scores, func(i, j int) bool { return scores[i] < scores[j] })
+	return scores[len(scores)/2]
+}
+
+func checkChunks(line string) (rune, []rune) {
+	var charToMatch rune
+	chars := make([]rune, 0)
+	for _, r := range line {
+		if strings.ContainsRune(openingChars, r) {
+			chars = append(chars, r)
+
+		} else if strings.ContainsRune(closingChars, r) {
+			charToMatch, chars = chars[len(chars)-1], chars[:len(chars)-1]
+			expectedOpening := getOpeningChar(r)
+			if charToMatch != expectedOpening {
+				return r, nil
+			}
+		} else {
+			log.Panicln("unhandled char", string(r))
+		}
+	}
+	return 0, chars
+}
+
+func getOpeningChar(r rune) rune {
+	return rune(openingChars[strings.IndexRune(closingChars, r)])
+}
+
+func getClosingChar(r rune) rune {
+	return rune(closingChars[strings.IndexRune(openingChars, r)])
+}
