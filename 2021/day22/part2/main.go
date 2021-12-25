e83d23b73e80c94f23c63099ca6751b978cce9b7
koszuta <koszutan94@gmail.com>
2021-12-25 00:32:34 -0700

    2021 day 22

--- 2021/day22/part2/main.go
+++ 2021/day22/part2/main.go
@@ -4,24 +4,22 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
-const expectedResult = 0
+const expectedResult = 1211172281877240
 
 /*
- *   --- Day 22:  ---
- *      --- Part Two ---
+ *   --- Day 22: Reactor Reboot ---
+ *          --- Part Two ---
  *
  *   https://adventofcode.com/2021/day/22#part2
  */
 
-var (
-	onCuboids  []cuboid
-	offCuboids []cuboid
-)
+var cuboids map[cuboid]int
 
 type cuboid struct {
 	x0, x1 int
@@ -34,8 +32,8 @@ func main() {
 		log.Println("took", time.Since(t))
 	}(time.Now())
 
-	x := doPart2()
-	log.Println(x)
+	nLitCubes := doPart2()
+	log.Println("the number of on cubes is", nLitCubes)
 }
 
 func doPart2() int {
@@ -43,57 +41,83 @@ func doPart2() int {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 
-	for i := 0; scanner.Scan(); i++ {
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
+	cuboids = make(map[cuboid]int)
+
+	rebootRegex := regexp.MustCompile(`-?\d+`)
+	for scanner.Scan() {
+		parts := strings.Split(scanner.Text(), " ")
 
 		on := parts[0] == "on"
+		nums := rebootRegex.FindAllString(parts[1], -1)
 
-		parts = strings.Split(parts[1], ",")
-
-		xParts := strings.Split(parts[0][2:], "..")
-		x0, _ := strconv.Atoi(xParts[0])
-		x1, _ := strconv.Atoi(xParts[1])
+		x0, _ := strconv.Atoi(nums[0])
+		x1, _ := strconv.Atoi(nums[1])
 		if x0 > x1 {
 			x0, x1 = x1, x0
 		}
-
-		yParts := strings.Split(parts[1][2:], "..")
-		y0, _ := strconv.Atoi(yParts[0])
-		y1, _ := strconv.Atoi(yParts[1])
+		y0, _ := strconv.Atoi(nums[2])
+		y1, _ := strconv.Atoi(nums[3])
 		if y0 > y1 {
 			y0, y1 = y1, y0
 		}
-
-		zParts := strings.Split(parts[2][2:], "..")
-		z0, _ := strconv.Atoi(zParts[0])
-		z1, _ := strconv.Atoi(zParts[1])
+		z0, _ := strconv.Atoi(nums[4])
+		z1, _ := strconv.Atoi(nums[5])
 		if z0 > z1 {
 			z0, z1 = z1, z0
 		}
 
+		newCuboid := cuboid{x0, x1, y0, y1, z0, z1}
+
+		change := make(map[cuboid]int)
+		for c, sign := range cuboids {
+			if intersection, intersects := intersection(c, newCuboid); intersects {
+				change[intersection] -= sign
+			}
+		}
 		if on {
-			onCuboids = append(onCuboids, cuboid{x0, x1, y0, y1, z0, z1})
-		} else {
-			offCuboids = append(offCuboids, cuboid{x0, x1, y0, y1, z0, z1})
+			change[newCuboid]++
+		}
+		for c, sign := range change {
+			cuboids[c] += sign
 		}
 	}
 
 	nOnCubes := 0
-
-	for j := 1; j < len(onCuboids); j++ {
-		for i := j + 1; i < len(onCuboids); i++ {
-
-		}
+	for c, sign := range cuboids {
+		nOnCubes += (c.volume() * sign)
 	}
-
 	return nOnCubes
 }
 
-func overlap(c1, c2 cuboid) []cuboid {
-    newCuboids = 
+func intersection(c1, c2 cuboid) (intersection cuboid, intersects bool) {
+	intersection = cuboid{
+		max(c1.x0, c2.x0),
+		min(c1.x1, c2.x1),
+		max(c1.y0, c2.y0),
+		min(c1.y1, c2.y1),
+		max(c1.z0, c2.z0),
+		min(c1.z1, c2.z1),
+	}
+	if intersection.x0 > intersection.x1 || intersection.y0 > intersection.y1 || intersection.z0 > intersection.z1 {
+		return cuboid{}, false
+	}
+	return intersection, true
 }
 
-func volume(c cuboid) int {
-	return c.x0*c.x1 + c.y0*c.y1 + c.z0*c.z1
+func (c *cuboid) volume() int {
+	return (c.x1 - c.x0 + 1) * (c.y1 - c.y0 + 1) * (c.z1 - c.z0 + 1)
+}
+
+func min(p, q int) int {
+	if p < q {
+		return p
+	}
+	return q
+}
+
+func max(p, q int) int {
+	if p > q {
+		return p
+	}
+	return q
 }
